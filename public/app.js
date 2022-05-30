4971dfa05f7522514286eca934d87087f0b84e9e
whiteball <chongjayden07@gmail.com>
2022-05-30 23:47:08 +0800

    initial add

---
+++ public/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function(e) {
+	const $selectors = {
+		"left": $("#left"),
+	}
+})
---
+++ public/parser.py
@@ -0,0 +1,5 @@
+## lightweight and simple parser for markdown contents to be translated into html document
+if __name__ == "__main__":
+	contents = ""
+	with open("contents.md", "r") as f:
+		contents = f.read();
